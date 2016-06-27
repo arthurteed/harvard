a3d5d4cdbe00edb64fc1cbdd28f7457ee4ff16cd
arthurteed <arthurteed@g.harvard.edu>
2016-06-27 16:54:11 -0400

    Final Product of Assignment 1

---
+++ Lecture3.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+let immutableString = "Hello, playground"
+
+var str = "Hello, playground"
+
+str = "What's up"
+
+var myInt = 42
+
+let myConstant = 50
+
+let implicitDouble = 70.0
+let explicitDouble : Double = 500.323
+ 
